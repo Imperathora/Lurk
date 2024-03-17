9f7068d06d9e422ab9bdaafe6a9182dc546f17a6
Imperathora <strongdayclear@gmail.com>
2024-03-17 18:28:49 +0100

    more interaction stuff

--- Lurk/Source/Lurk/Private/Systems/LCharacter.cpp
+++ Lurk/Source/Lurk/Private/Systems/LCharacter.cpp
@@ -3,6 +3,45 @@
 // Sets default values
 ALCharacter::ALCharacter()
 {
-	// Set this character to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ALCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultInputMapping, 0);
+		}
+	}
+}
+
+
+void ALCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	auto playerController = Cast<APlayerController>(GetController());
+
+	// Get the local player enhanced input subsystem
+	auto eiSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer());
+	//Add the input mapping context
+	eiSubsystem->AddMappingContext(DefaultInputMapping, 0);
+
+	// Get the EnhancedInputComponent
+	auto playerEIcomponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	/*if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+
+	}*/
+}
+
+UInputAction* ALCharacter::GetInteractAction()
+{
+	return Input_Interact;
 }
--- Lurk/Source/Lurk/Public/Systems/LCharacter.h
+++ Lurk/Source/Lurk/Public/Systems/LCharacter.h
@@ -2,6 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 #include "LCharacter.generated.h"
 
 UCLASS()
@@ -11,4 +15,16 @@ class LURK_API ALCharacter : public ACharacter
 
 public:
 	ALCharacter();
+
+	virtual void BeginPlay() override;
+	void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent);
+
+	UInputAction* GetInteractAction();
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultInputMapping;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputAction* Input_Interact;
 };
