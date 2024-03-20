c19a03c77b12249a9dd6d94e176568d788f62536
Imperathora <strongdayclear@gmail.com>
2024-03-20 12:21:07 +0100

    added interaction detection

--- Lurk/Source/Lurk/Private/Systems/Interaction/LInteractorComponent.cpp
+++ Lurk/Source/Lurk/Private/Systems/Interaction/LInteractorComponent.cpp
@@ -34,9 +34,64 @@ void ULInteractorComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 
 void ULInteractorComponent::UpdateInteractionCandidates()
 {
+	TRACE_CPUPROFILER_EVENT_SCOPE(ULInteractorComponent::UpdateInteractionCandidates);
+
+	const APlayerCameraManager* CameraManager = GetWorld()->GetFirstPlayerController()->PlayerCameraManager;
+	const FVector CameraLocation = CameraManager->GetCameraLocation();
+	const FVector CameraForward = CameraManager->GetCameraRotation().Vector();
+
+	const FVector Start = CameraLocation;
+	const FVector End = Start + CameraForward * InteractionDistance;
+	const float Radius = InteractionCircumference;
+	const float HalfHeight = InteractionCircumference;
+
+	const bool bTraceComplex = false;
+	const bool bIgnoreSelf = true;
+
+	const TArray<AActor*> ActorsToIgnore;
+	
+	const ETraceTypeQuery TraceChannel = UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_Visibility);
+	const EDrawDebugTrace::Type DrawDebugType = EDrawDebugTrace::ForOneFrame;
+	const FLinearColor TraceColor = FLinearColor(FColorList::Orange);
+	const FLinearColor TraceHitColor = FLinearColor(FColorList::Red);
+
+	TArray<FHitResult> OutHits;
+	UKismetSystemLibrary::CapsuleTraceMulti(this, Start, End, Radius, HalfHeight, TraceChannel, bTraceComplex, ActorsToIgnore, DrawDebugType, OutHits, bIgnoreSelf, TraceColor, TraceHitColor);
+
+
+	TArray<ULInteractionComponent*> CandidateInteractables;
+
+	FString DebugState = TEXT("Current Candidates: \n");
+
+	for (const FHitResult& Hit : OutHits)
+	{
+		const AActor* HitActor = Hit.GetActor();
+		ULInteractionComponent* HitInteractable = HitActor ? HitActor->FindComponentByClass<ULInteractionComponent>() : nullptr;
+		if (HitInteractable)
+		{
+			CandidateInteractables.Add(HitInteractable);
+			DebugState.Appendf(TEXT(" % s\n"), *HitActor->GetHumanReadableName());
+		}
+
+		GEngine->AddOnScreenDebugMessage(INDEX_NONE, 0.f, FColor::Red, DebugState);
+
+		if (!CandidateInteractables.IsEmpty())
+		{
+			InteractionTarget = CandidateInteractables[0];
+		}
+		else
+		{
+			InteractionTarget = nullptr;
+		}
+	}
+
 }
 
 void ULInteractorComponent::PerformInteraction()
 {
+	if (InteractionTarget)
+	{
+		InteractionTarget->TryInteract();
+	}
 }
 
